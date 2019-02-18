3cd2418505bb5259775cadc23fa9f906aecd5f40
a2008ok <40367357+a2008ok@users.noreply.github.com>
2019-02-18 18:25:21 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,42 +33,53 @@ e.style.fontSize = 0.04506 * x + "px";
 function abc1(){
     if(a.value.length == 0){ 
         alert("请输入姓名！"); 
-        
-        var canvas = document.getElementById("myCanvas");
-        var ctx=canvas.getContext("2d");
-
-        var img1=document.getElementById("answerimg");
-        img1.crossOrigin = "anonymous";
-        var img2=document.getElementById("qianwen");
-        img2.crossOrigin = "anonymous";
-
-        ctx.drawImage(img1,0,0);
-        ctx.drawImage(img2,0,0);
-        ctx.font = "bold 54px sans-serif";
-        ctx.textAlign = "center";
-        ctx.fillStyle = "#f9f5f4";
-        ctx.fillText("甄伟前牛逼",375,76,278);
-        
-        var output = document.getElementById("test");
-        output.src = canvas.toDataURL("image/png");
-        
     }
     else{
         /*进入抽签页动画过渡*/
-        setTimeout('$("#begin").fadeOut(100);', 100);
-        setTimeout('$("#process").fadeIn(100);', 205);
+        setTimeout('$("#begin").fadeOut(1000);', 100);
+        setTimeout('$("#process").fadeIn(1300);', 1100);
         /*随机抽选签文*/
         var qianwen = document.getElementById("qianwen");
-        var f = Math.floor(Math.random()*3);
+        var f = Math.floor(Math.random()*13);
         if (f == 0){
             qianwen.src = "./picture/1.png";
         }
         else if (f == 1){
-            qianwen.src = "./picture/1.png";
+            qianwen.src = "./picture/2.png";
+        }
+        else if (f == 2){
+            qianwen.src = "./picture/3.png";
+        }
+        else if (f == 3){
+            qianwen.src = "./picture/4.png";
+        }
+        else if (f == 4){
+            qianwen.src = "./picture/5.png";
+        }
+        else if (f == 5){
+            qianwen.src = "./picture/6.png";
+        }
+        else if (f == 6){
+            qianwen.src = "./picture/7.png";
+        }
+        else if (f == 7){
+            qianwen.src = "./picture/8.png";
+        }
+        else if (f == 8){
+            qianwen.src = "./picture/9.png";
+        }
+        else if (f == 9){
+            qianwen.src = "./picture/10.png";
+        }
+        else if (f == 10){
+            qianwen.src = "./picture/11.png";
+        }
+        else if (f == 11){
+            qianwen.src = "./picture/12.png";
         }
         else{
-            qianwen.src = "./picture/1.png"
-        };
+            qianwen.src = "./picture/13.png"
+        }
     };
 };
 
@@ -81,10 +92,27 @@ function abc2(){
     $("#qiantong").animate({top:0.06*x+"px"});
     $("#qiantong").animate({top:0.13266*x+"px"});
     /*进入解签页动画过渡*/
-    setTimeout('$("#process").fadeOut(100);', 3000);
-    setTimeout('$("#answer").fadeIn(100);', 3105);
+    setTimeout('$("#process").fadeOut(800);', 3300);
+    setTimeout('$("#answer").fadeIn(1300);', 4100);
     /*替换解签页姓名*/
     e.innerHTML = a.value;
 };
 
 
+        /*var canvas = document.getElementById("myCanvas");
+        var ctx=canvas.getContext("2d");
+
+        var img1=document.getElementById("answerimg");
+        img1.crossOrigin = "anonymous";
+        var img2=document.getElementById("qianwen");
+        img2.crossOrigin = "anonymous";
+
+        ctx.drawImage(img1,0,0);
+        ctx.drawImage(img2,0,0);
+        ctx.font = 'bold 54px sans-serif';
+        ctx.textAlign = "center";
+        ctx.fillStyle = "#f9f5f4";
+        ctx.fillText("甄伟前牛逼",375,76,278);
+        
+        var output = document.getElementById("test");
+        output.src = canvas.toDataURL("image/png");*/
