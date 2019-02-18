a2a05d16aa3df73eecae9678bbd8e972126dd417
a2008ok <40367357+a2008ok@users.noreply.github.com>
2019-02-18 20:32:15 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@ function abc1(){
         ctx.font = 'bold 54px sans-serif';
         ctx.textAlign = "center";
         ctx.fillStyle = "#f9f5f4";
-        ctx.fillText("甄伟前牛逼",375,76,278);
+        ctx.fillText(a.value,375,76,278);
         
         var output = document.getElementById("test");
         output.src = canvas.toDataURL("image/png");
